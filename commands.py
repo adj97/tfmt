400f0366f641497f944d20460befa5e6482cc061
Andrew Dixon <andrew.dixon@capgemini.com>
2021-12-13 11:13:56 +0000

    main: Working

--- commands.py
+++ commands.py
@@ -1,8 +1,9 @@
-from helpers import *
+from helpers import get_continue_conditions, patterns, output, input_fmt, yesno, printlog, summary
 from cli import *
-from re import compile, sub
+from re import sub
 from tabulate import tabulate
 from copy import copy
+import pyperclip
 
 
 def tfmt(filename_input):
@@ -11,7 +12,8 @@ def tfmt(filename_input):
     if len(arg_decomp)==1:
         if arg_decomp[0] == "clip":
             output("Reading from clipboard")
-            raise Exception("Feature not supported yet")
+            clipboard = True
+            cliptext = pyperclip.paste()
         else:
             # Assume txt file
             filename = filename_input + ".txt"
@@ -27,16 +29,19 @@ def tfmt(filename_input):
         raise Exception("Unrecognised input: \"" + filename_input + "\"")
 
     # Read the file
-    try:
-        f = [l for l in open(filename, "r")]
-    except:
-        output("filename: \"" + filename + "\" does not exist in the current directory", "e")
-        exit()
+    if not clipboard:
+        try:
+            f = [l for l in open(filename, "r")]
+        except:
+            output("filename: \"" + filename + "\" does not exist in the current directory", "e")
+            exit()
+    else:
+        f = cliptext.split("\n")
 
     # Check for some terraform stuff
     tf_strings = [
         "Terraform will perform the following actions:",
-        "Plan","add","change","destroy", "pigz"
+        "Plan","add","change","destroy"
     ]
     check = 100 * sum([checkstring in ''.join(f) for checkstring in tf_strings]) / len(tf_strings)
     if check != 100:
@@ -46,43 +51,20 @@ def tfmt(filename_input):
             output("This is {0}% a tf output file ...".format(check), "h")
             output("I'm happy to carry on, but I might not do a great job if this isn't a tf output file", "h")
             output("Do you want me to carry on anyway?", "h")
-
-    # Patterns array does some data clensing according to the following:
-    # "digits"  - remove numbers at the end of lines"
-    # "newline" - remove newlines
-    # "azs"     - merge all the availability zone lines together
-    # "plus"    - remove plus sign at the start
-    patterns = {
-        "digits" : ["\[\d+\]", ""], 
-        "newline" : ["\\n", ""],
-        "azs" : ["_az_\d", "_az_x"], 
-        "plus" : ["\+ ", ""]
-    }
-
-    for p in patterns:
-        patterns[p][0] = compile(patterns[p][0])
+            if not yesno():
+                output("Stopping")
+                exit()
 
     # construct and filter lines array
     lines = []
     for line in f:
 
-        # Continue conditions does some data clensing according to the following:
-        # Boolean array where the following if statement will ignore a line if true
-        # [0] - blank lines
-        # [1] - added/created property lines
-        # [2] - terraform std output
-        # [3] - terraform summary line
-        continue_conditions = [
-            line == "\n", 
-            line[0:6]==" "*6, 
-            "Terraform will perform the following actions:" in line,
-            any(substring in line for substring in ["Plan: "," to add, "," to change, "," to destroy"])
-        ]
+        continue_conditions = get_continue_conditions(line)
 
         # skip or keep each line
         if any(continue_conditions):
             if continue_conditions[3]:
-                print("\n" + line)
+                printlog("\n" + line)
             continue
         else:
             for p in patterns:
@@ -94,6 +76,11 @@ def tfmt(filename_input):
     groups = [[line for line in lines if line==compare_line] for i, compare_line in enumerate(lines) if compare_line not in lines[:i]]
 
     # Output
-    headers = ["Resource (grouped)", "Count"]
-    data = [[group[0], len(group)] for group in groups]
-    print("\n" + tabulate(data, headers) + "\n")
+    headers = ["Count", "Resource Changed (grouped)"]
+    data = [[len(group), group[0]] for group in groups]
+    printlog("\n" + tabulate(data, headers) + "\n")
+
+    # Copy to clipboard
+    output("Copy to clipboard?")
+    if yesno():
+        pyperclip.copy(''.join(summary))
--- helpers.py
+++ helpers.py
@@ -1,9 +1,61 @@
+from re import compile
+
 output_types={
     "o":" output",
     "e":"  error",
     "h":"   help"
 }
 
-def output(output, type="o"):
-    output = ["tfmt", output_types[type], str(output)]
-    print(": ".join(output))
+def output(text, type="o"):
+    print(cli_format(text, type))
+
+def input_fmt(text, type="h"):
+    input(cli_format(text, type))
+
+def cli_format(text, type):
+    return ": ".join(["tfmt", output_types[type], str(text)])
+
+# Patterns array does some data clensing according to the following:
+# "digits"  - remove numbers at the end of lines"
+# "newline" - remove newlines
+# "azs"     - merge all the availability zone lines together
+# "plus"    - remove plus sign at the start
+patterns = {
+    "digits" : ["\[\d+\]", ""], 
+    "newline" : ["\\n", ""],
+    "azs" : ["_az_\d", "_az_x"], 
+    "plus" : ["\+ ", ""]
+}
+
+for p in patterns:
+    patterns[p][0] = compile(patterns[p][0])
+    
+# Continue conditions does some data clensing according to the following:
+# Boolean array where the following if statement will ignore a line if true
+# [0] - blank lines
+# [1] - added/created property lines
+# [2] - terraform std output
+# [3] - terraform summary line
+def get_continue_conditions(line):
+    return [
+        line == "\n", 
+        line[0:6]==" "*6, 
+        "Terraform will perform the following actions:" in line,
+        any(substring in line for substring in ["Plan: "," to add, "," to change, "," to destroy"])
+    ]
+
+def yesno():
+    try:
+        if input_fmt("[Y/n] ").lower() in ["n", "no"]:
+            return False
+        else:
+            return True
+    except AttributeError as e:
+        # Probably default enter
+        return True
+
+summary = []
+
+def printlog(text):
+    print(text)
+    summary.append(text)
